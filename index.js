5ddb25683b8c8736d8c13305db02a56b4bb643df
Muhammad Usman <awanask086@gmail.com>
2026-08-17 12:51:02 +0500

    index

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ require("dotenv").config();
 const cookieParser = require("cookie-parser");
 const { dbconnection } = require("./connection");
 const app = express();
-const {authMiddleware} = require("./middleware/authentication")
-const {authorMiddleware} = require("./middleware/authorization")
+const { authMiddleware } = require("./middleware/authentication");
+const { authorMiddleware } = require("./middleware/authorization");
 const userRoutes = require("./routes/authRoutes");
+const productRoutes = require("./routes/productRoutes")
 app.use(express.json());
-app.use(express.urlencoded({express:false}))
-app.use(cookieParser())
+app.use(express.urlencoded({ express: false }));
+app.use(cookieParser());
 dbconnection(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log(err));
@@ -16,15 +17,15 @@ dbconnection(process.env.MONGO_URI)
 app.get("/check", (req, res) => {
   return res.end(`checking for ecommerce backend flow `);
 });
-app.get("/profile", authMiddleware, (req, res)=>{
-  return res.json({msg:`we are on profile`})
-})
+app.get("/profile", authMiddleware, (req, res) => {
+  return res.json({ msg: `welcome to profile` });
+});
 
-app.get("/admin", authMiddleware,authorMiddleware ,(req, res)=>{
-  return res.json({msg:`access denied`})
-})
+app.get("/admin", authMiddleware, authorMiddleware, (req, res) => {
+  return res.json({ msg: `welcome admin` });
+});
 app.use("/", userRoutes);
-
+app.use("/product", productRoutes)
 app.listen(process.env.PORT, (req, res) => {
   console.log(`server connected`);
 });
