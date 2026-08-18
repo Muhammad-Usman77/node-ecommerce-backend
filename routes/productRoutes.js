1a255f7edaa608ac533044ffad35c925582d2db4
Muhammad Usman <awanask086@gmail.com>
2026-08-18 08:40:33 +0500

    product routes

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -2,8 +2,14 @@ const express = require("express")
 const {authMiddleware } = require("../middleware/authentication")
 const {authorMiddleware} = require("../middleware/authorization");
 const { createProduct, getProducts, getProductById, deleteProductById , updateProductByIdPatch, searchByName, filterByCategory, productFilter, filterByPrice, pagenation, sorting} = require("../controller/product");
+const { addToCart, getCart, updateCartQuantity, removeFromCart, clearCart } = require("../controller/cart");
 
 const router = express.Router();
+router.post("/post", authMiddleware,authorMiddleware, addToCart)
+router.get("/getCart", authMiddleware, authorMiddleware, getCart)
+router.patch("/cart/update", authMiddleware, authorMiddleware, updateCartQuantity)
+router.delete("/remove", authMiddleware, authorMiddleware, removeFromCart)
+router.delete("/clear", authMiddleware, authorMiddleware, clearCart)
 router.get("/search", authMiddleware, authorMiddleware, searchByName)
 router.get("/filterByCategory", authMiddleware, authorMiddleware, filterByCategory),
 router.get("/proudctFilter", authMiddleware, authorMiddleware, productFilter)
@@ -15,5 +21,4 @@ router.get("/getAll", authMiddleware, authorMiddleware, getProducts)
 router.get("/:id", authMiddleware, authorMiddleware, getProductById)
 router.delete("/:id", authMiddleware, authorMiddleware, deleteProductById)
 router.patch("/:id", authMiddleware, authorMiddleware, updateProductByIdPatch)
-
 module.exports = router;
