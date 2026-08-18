3bfa20d3082ece171d1fc4b7946a1f5c51ed5803
Muhammad Usman <awanask086@gmail.com>
2026-08-18 12:49:31 +0500

    order Model

---
+++ model/order.js
@@ -0,0 +1,54 @@
+const mongoose = require("mongoose");
+
+const orderSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+    products: [
+      {
+        productId: {
+          type: mongoose.Schema.Types.ObjectId,
+          required: true,
+          ref: "productModel",
+        },
+        name: {
+          type: String,
+          required: true,
+        },
+        quantity: {
+          type: Number,
+          required: true,
+        },
+        price: {
+          type: String,
+          required: true,
+        },
+        totalPrice: {
+          type: String,
+          required: true,
+        },
+      },
+    ],
+    totalAmount: {
+      type: Number,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["pending", "confirmed", "shipped", "delivered", "cancelled"],
+      default: "pending",
+    },
+    shippingAddress: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
+
+const Order = mongoose.model("order", orderSchema);
+
+module.exports = Order;
