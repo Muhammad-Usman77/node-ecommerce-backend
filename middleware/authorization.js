0c3463a67110c1175478bfcf56a3fe55fa5c790f
Muhammad Usman <awanask086@gmail.com>
2026-08-19 21:56:51 +0500

    authorization middleware

--- middleware/authorization.js
+++ middleware/authorization.js
@@ -2,14 +2,16 @@ const User = require("../model/authModel");
 
 async function authorMiddleware(req, res, next) {
   const user = await User.findById(req.userId);
-
+  console.log("USER:", user);
+  console.log("ROLE:", user?.role);
   if (!user) {
     return res.json({ msg: `user not found` });
   }
 
   if (user.role !== "admin") {
-    return res.json({ msg: `access denied for admin` });
+    return res.json({ msg: `access denied ` });
   }
+   console.log("ADMIN ACCESS GRANTED");
   next();
 }
 
