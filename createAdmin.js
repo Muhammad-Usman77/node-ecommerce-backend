1b654882f00900d4a011a5a207637166072bc2a0
Muhammad Usman <awanask086@gmail.com>
2026-08-19 21:56:05 +0500

    create admin

--- createAdmin.js
+++ createAdmin.js
@@ -1,40 +1,40 @@
-const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
-const User = require("./model/authModel");
-require("dotenv").config();
+// const mongoose = require("mongoose");
+// const bcrypt = require("bcrypt");
+// const User = require("./model/authModel");
+// require("dotenv").config();
 
-async function createAdmin() {
-  try {
-    await mongoose.connect(process.env.MONGO_URI);
+// async function createAdmin() {
+//   try {
+//     await mongoose.connect(process.env.MONGO_URI);
 
-    const existingAdmin = await User.findOne({
-      role: "admin",
-    });
+//     const existingAdmin = await User.findOne({
+//       role: "admin",
+//     });
 
-    if (existingAdmin) {
-      console.log("Admin already exists");
-      process.exit();
-    }
+//     if (existingAdmin) {
+//       console.log("Admin already exists");
+//       process.exit();
+//     }
 
-    const hashedPassword = await bcrypt.hash("Admin@123", 10);
+//     const hashedPassword = await bcrypt.hash("Admin@123", 10);
 
-    await User.create({
-      name: "Admin",
-      email: "admin@example.com",
-      password: hashedPassword,
-      role: "admin",
-    });
+//     await User.create({
+//       name: "Admin",
+//       email: "admin@example.com",
+//       password: hashedPassword,
+//       role: "admin",
+//     });
 
-    console.log("Admin created successfully");
+//     console.log("Admin created successfully");
 
-    process.exit();
-  } catch (error) {
-    console.log(error);
-    process.exit(1);
-  }
-}
+//     process.exit();
+//   } catch (error) {
+//     console.log(error);
+//     process.exit(1);
+//   }
+// }
 
-createAdmin();
+// createAdmin();
 
-//node createAdmin.js
-//es commnad ko aik dafa run krna hai. phir aik project makn dobara nh.
+// //node createAdmin.js
+// //es commnad ko aik dafa run krna hai. phir aik project makn dobara nh.
