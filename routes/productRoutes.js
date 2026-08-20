f4fddf26b278146aa7a8470b5feef901402756f6
Muhammad Usman <awanask086@gmail.com>
2026-08-20 12:23:00 +0500

    productRoutes

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { authMiddleware } = require("../middleware/authentication");
 const { authorMiddleware } = require("../middleware/authorization");
+const upload = require("../middleware/uploads");
 const {
   createProduct,
   getProducts,
@@ -17,25 +18,21 @@ const {
 
 const router = express.Router();
 
+
+//searching
 router.get("/search", authMiddleware, searchByName);
-(router.get(
-  "/filterByCategory",
-  authMiddleware,
-
-  filterByCategory,
-),
-  router.get(
-    "/proudctFilter",
-    authMiddleware,
-
-    productFilter,
-  ));
+(router.get("/filterByCategory", authMiddleware, filterByCategory,),
+  router.get( "/proudctFilter", authMiddleware, productFilter,));
 router.get("/filterByPrice", authMiddleware, filterByPrice);
 router.get("/pagenation", authMiddleware, pagenation);
 router.get("/sort", authMiddleware, sorting);
-router.post("/create", authMiddleware, authorMiddleware, createProduct);
+
+//crud
+router.post("/create", authMiddleware, authorMiddleware, upload.array("images", 5), createProduct);
 router.get("/getAll", authMiddleware, getProducts);
 router.get("/:id", authMiddleware, getProductById);
 router.delete("/:id", authMiddleware, authorMiddleware, deleteProductById);
 router.patch("/:id", authMiddleware, authorMiddleware, updateProductByIdPatch);
+
+//router.
 module.exports = router;
