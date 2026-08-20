bee49587436553c7520076a200eef4b55453e9d6
Muhammad Usman <awanask086@gmail.com>
2026-08-20 12:21:51 +0500

    wishlist controller

---
+++ controller/wishlist.js
@@ -0,0 +1,87 @@
+const Wishlist = require("../model/wishlist");
+const Product = require("../model/productModel");
+
+async function addToWishlist(req, res) {
+
+  const { productId } = req.body;
+
+  // 1. Check productId
+  if (!productId) {
+    return res.status(400).json({
+      msg: "productId is required",
+    });
+  }
+
+  // 2. Check product exists
+  const product = await Product.findById(productId);
+
+  if (!product) {
+    return res.status(404).json({
+      msg: "Product not found",
+    });
+  }
+
+  // 3. Check already in wishlist
+  const alreadyExists = await Wishlist.findOne({
+    userId: req.userId,
+    productId: productId,
+  });
+
+  if (alreadyExists) {
+    return res.status(400).json({
+      msg: "Product already exists in wishlist",
+    });
+  }
+
+  // 4. Create wishlist
+  const wishlist = await Wishlist.create({
+    userId: req.userId,
+    productId: productId,
+  });
+
+  return res.status(201).json({
+    msg: "Product added to wishlist",
+    data: wishlist,
+  });
+}
+async function getMyWishlist(req, res) {
+
+  const wishlist = await Wishlist.find({
+    userId: req.userId,
+  }).populate("productId");
+
+  return res.json({
+    msg: "Wishlist fetched successfully",
+    data: wishlist,
+  });
+}
+async function removeFromWishlist(req, res) {
+
+  const { productId } = req.body;
+
+  if (!productId) {
+    return res.status(400).json({
+      msg: "productId is required",
+    });
+  }
+
+  const wishlist = await Wishlist.findOneAndDelete({
+    userId: req.userId,
+    productId: productId,
+  });
+
+  if (!wishlist) {
+    return res.status(404).json({
+      msg: "Product not found in your wishlist",
+    });
+  }
+
+  return res.json({
+    msg: "Product removed from wishlist",
+    data: wishlist,
+  });
+}
+
+module.exports = {
+  addToWishlist, getMyWishlist, removeFromWishlist
+};
