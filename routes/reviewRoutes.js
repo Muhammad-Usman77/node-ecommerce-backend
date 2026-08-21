e8f3eab1d568a5f80bef0884cd544de246235c99
Muhammad Usman <awanask086@gmail.com>
2026-08-21 07:01:07 +0500

    review routes

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -4,9 +4,12 @@ const { authMiddleware } = require("../middleware/authentication");
 
 const { createReview, getProductReviews, updateReview, deleteReview } = require("../controller/review");
 
+const {reviewSchema} = require("../validation/reviewValidation")
+const validate = require("../middleware/validateMiddleware")
+
 const router = express.Router();
 
-router.post("/create", authMiddleware, createReview);
+router.post("/create", authMiddleware, validate(reviewSchema), createReview);
 router.get("/product/:productId", authMiddleware, getProductReviews);
 router.patch("/:id", authMiddleware, updateReview)
 router.delete("/:id", authMiddleware, deleteReview)
