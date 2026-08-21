5d61b82a7e49ddb38d7c3df04a10aa2ee3110f1e
Muhammad Usman <awanask086@gmail.com>
2026-08-21 09:38:42 +0500

    wishlist routes

--- routes/wishlistRoutes.js
+++ routes/wishlistRoutes.js
@@ -1,7 +1,8 @@
 const express = require("express");
 
 const { authMiddleware } = require("../middleware/authentication");
-
+const { testError } = require("../controller/wishlist");
+const asyncHandler = require("../middleware/asyncHandler");
 const { addToWishlist, getMyWishlist, removeFromWishlist } = require("../controller/wishlist");
 
 const router = express.Router();
@@ -11,4 +12,6 @@ router.post("/add", authMiddleware, addToWishlist);
 router.get("/my", authMiddleware, getMyWishlist);
 
 router.delete("/remove", authMiddleware, removeFromWishlist)
+
+router.get("/test-error", asyncHandler(testError))
 module.exports = router;
