f572c0d336309dc241399a964e6b878e8e614550
Muhammad Usman <awanask086@gmail.com>
2026-08-21 07:00:33 +0500

    order routes

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,12 +1,14 @@
 const express = require("express")
 const {authMiddleware } = require("../middleware/authentication")
-const {authorMiddleware} = require("../middleware/authorization");
+const {authorMiddleware} = require("../middleware/authorization")
+const {orderSchema} = require("../validation/orderValidation")
+const validate = require("../middleware/validateMiddleware")
 const { createOrder, getAllOrder, getOrderById, orderCancell, adminGetAllOrders, adminUpdateOrderStatus } = require("../controller/order");
 const router = express.Router();
 
 
 router.get("/getAllOrder", authMiddleware, getAllOrder)
-router.post("/createOrder", authMiddleware, createOrder)
+router.post("/createOrder", authMiddleware, validate(orderSchema), createOrder)
 
 router.get("/:id", authMiddleware, getOrderById)
 router.patch("/:id", authMiddleware, orderCancell)
