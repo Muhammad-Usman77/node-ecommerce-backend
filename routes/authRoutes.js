1ad4d35ea04860dbaaac6fbfe696319ef3150e69
Muhammad Usman <awanask086@gmail.com>
2026-08-21 07:00:22 +0500

    auth routes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,3 +1,5 @@
+const { signupSchema, loginSchema } = require("../validation/authvalidation");
+const validate = require("../middleware/validateMiddleware");
 const express = require("express");
 const {
   userCreate,
@@ -6,8 +8,8 @@ const {
 } = require("../controller/authController");
 const router = express.Router();
 
-router.post("/register", userCreate);
-router.post("/login", userLogin);
+router.post("/register",validate(signupSchema), userCreate);
+router.post("/login",validate(loginSchema), userLogin);
 router.post("/logout", userLogout);
 
 module.exports = router;
