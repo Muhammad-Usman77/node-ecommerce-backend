e2610aeeba7fadcdeb21ebe5dcf1c149179b1be9
Muhammad Usman <awanask086@gmail.com>
2026-08-21 05:52:19 +0500

    index

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ const cartRoutes = require("./routes/cartRoutes")
 const orderRoutes = require("./routes/orderRoutes")
 const reviewRoutes = require("./routes/reviewRoutes")
 const wishlistRoutes = require("./routes/wishlistRoutes")
+const addressRoutes = require("./routes/addressRoutes")
+const paymentRoutes = require("./routes/paymentRoutes");
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -34,6 +36,8 @@ app.use("/cart", cartRoutes)
 app.use("/order", orderRoutes)
 app.use("/review", reviewRoutes)
 app.use("/wishlist", wishlistRoutes)
+app.use("/address", addressRoutes)
+app.use("/payment", paymentRoutes);
 app.listen(process.env.PORT, (req, res) => {
   console.log(`server connected`);
 });
