474402241bbb7ea803ed8a447bd33f64d3518504
Muhammad Usman <awanask086@gmail.com>
2026-08-22 06:53:38 +0500

    index

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 const express = require("express");
+const helmet = require("helmet")
+const cors = require("cors");
+const rateLimit = require("express-rate-limit");
 require("dotenv").config();
+if (!process.env.JWT_SECRET) {
+  throw new Error("JWT_SECRET is not defined");
+}
 const cookieParser = require("cookie-parser");
 const { dbconnection } = require("./connection");
 const app = express();
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  message: {
+    success: false,
+    message: "Too many requests, please try again later.",
+  },
+});
 const { authMiddleware } = require("./middleware/authentication");
 const { authorMiddleware } = require("./middleware/authorization");
 const errorMiddleware = require("./middleware/errorMiddleware")
 const notfoundMiddleware = require("./middleware/notfoundMiddleware");
+
 const userRoutes = require("./routes/authRoutes");
 const productRoutes = require("./routes/productRoutes");
 const cartRoutes = require("./routes/cartRoutes")
@@ -15,6 +30,15 @@ const reviewRoutes = require("./routes/reviewRoutes")
 const wishlistRoutes = require("./routes/wishlistRoutes")
 const addressRoutes = require("./routes/addressRoutes")
 const paymentRoutes = require("./routes/paymentRoutes");
+
+app.use(helmet());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
+app.use(limiter);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -47,3 +71,17 @@ app.use(errorMiddleware)
 app.listen(process.env.PORT, (req, res) => {
   console.log(`server connected`);
 });
+
+
+/*
+
+security:
+
+Helmet        → Security headers
+CORS          → Control frontend origins
+Rate Limit    → Too many requests block
+Joi           → Invalid input control
+bcrypt        → Password protection
+httpOnly      → Cookie security
+.env          → Secrets protection
+*/
