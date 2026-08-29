a8636b5cc177974881cac46de7586ec4f298b761
Muhammad Usman <awanask086@gmail.com>
2026-08-29 23:03:16 +0500

    product routes

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -2,6 +2,8 @@ const express = require("express");
 const { authMiddleware } = require("../middleware/authentication");
 const { authorMiddleware } = require("../middleware/authorization");
 const { productSchema, updateProductSchema } = require("../validation/productValidation");
+console.log("PRODUCT SCHEMA:", productSchema);
+console.log("UPDATE PRODUCT SCHEMA:", updateProductSchema);
 const validate = require("../middleware/validateMiddleware");
 const upload = require("../middleware/uploads");
 const {
@@ -23,18 +25,41 @@ const router = express.Router();
 
 //searching
 router.get("/search", authMiddleware, searchByName);
-(router.get("/filterByCategory", authMiddleware, filterByCategory,),
-  router.get( "/proudctFilter", authMiddleware, productFilter,));
+router.get("/filterByCategory", authMiddleware, filterByCategory),
+  router.get( "/proudctFilter", authMiddleware, productFilter);
 router.get("/filterByPrice", authMiddleware, filterByPrice);
 router.get("/pagenation", authMiddleware, pagenation);
 router.get("/sort", authMiddleware, sorting);
 
 //crud
-router.post("/create", authMiddleware, authorMiddleware, upload.array("images", 5), validate(productSchema), createProduct);
-router.get("/getAll", authMiddleware, getProducts);
+router.post(
+  "/create",
+  authMiddleware,
+  authorMiddleware,
+  upload.array("images", 5),
+
+  (req, res, next) => {
+    console.log("===== AFTER MULTER =====");
+    console.log("BODY:", req.body);
+    console.log("FILES:", req.files);
+    next();
+  },
+
+  validate(productSchema),
+
+  (req, res, next) => {
+    console.log("===== AFTER VALIDATION =====");
+    console.log("BODY AFTER VALIDATION:", req.body);
+    next();
+  },
+
+  createProduct
+);
+//router.post("/create", authMiddleware, authorMiddleware, upload.array("images", 5), validate(productSchema), createProduct);
+router.get("/getAll", authMiddleware,  getProducts);
 router.get("/:id", authMiddleware, getProductById);
 router.delete("/:id", authMiddleware, authorMiddleware, deleteProductById);
-router.patch("/:id", authMiddleware, authorMiddleware, validate(updateProductSchema), updateProductByIdPatch);
+router.patch("/:id", authMiddleware, authorMiddleware,upload.array("images", 5), validate(updateProductSchema), updateProductByIdPatch);
 
 //router.
 module.exports = router;
