2e22a7ed213f873ef92b2ea055a81dc67df4a67b
Muhammad Usman <awanask086@gmail.com>
2026-08-29 23:03:03 +0500

    validate middleware

--- middleware/validateMiddleware.js
+++ middleware/validateMiddleware.js
@@ -13,8 +13,42 @@
 // }
 
 // module.exports = validate;
+
+
+//2
+// function validate(schema, source = "body") {
+//   return (req, res, next) => {
+
+//     const data = req[source];
+
+//     const { error, value } = schema.validate(data, {
+//       abortEarly: false,
+//       stripUnknown: true,
+//     });
+
+//     if (error) {
+//       return res.status(400).json({
+//         msg: "Validation failed",
+//         errors: error.details.map((detail) => detail.message),
+//       });
+//     }
+
+//     req[source] = value;
+
+//     next();
+//   };
+// }
+
+// module.exports = validate;
+
+
+
+//3
 function validate(schema, source = "body") {
   return (req, res, next) => {
+
+    console.log("===== VALIDATION START =====");
+    console.log("DATA BEFORE VALIDATION:", req[source]);
 
     const data = req[source];
 
@@ -22,6 +56,9 @@ function validate(schema, source = "body") {
       abortEarly: false,
       stripUnknown: true,
     });
+
+    console.log("VALIDATION ERROR:", error);
+    console.log("VALIDATION VALUE:", value);
 
     if (error) {
       return res.status(400).json({
@@ -32,12 +69,14 @@ function validate(schema, source = "body") {
 
     req[source] = value;
 
+    console.log("===== VALIDATION SUCCESS =====");
+    console.log("REQ BODY AFTER VALIDATION:", req[source]);
+
     next();
   };
 }
 
 module.exports = validate;
-
 /*
 validate(productIdSchema, "params")
 validate(searchSchema, "query")
