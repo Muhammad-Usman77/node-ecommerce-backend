e7874b46f692535311f5272a4bdc30c704981dc7
Muhammad Usman <awanask086@gmail.com>
2026-08-29 23:02:22 +0500

    index

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.use(
   })
 );
 app.use(limiter);
-
+app.use("/uploads", express.static("uploads"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
